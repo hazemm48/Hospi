bb23cb332805ddd3dc15998d3a8f4411122a8c64
Hazem Mohamed <hazem1abc@gmail.com>
2023-05-25 21:49:45 +0300

    s

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -2,9 +2,14 @@ import React from "react";
 
 let LoadingSpinner = () => {
   return (
-    <div className="main-content">
+    <div className="main-content" style={{ alignContent: "center" }}>
       <div className="spinner-container">
-        <div className="loading-spinner"></div>
+        <div className="loading-spinner">
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
       </div>
     </div>
   );
